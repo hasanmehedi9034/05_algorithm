734899324a2e244d1d4951e5b4abd0192aea30b1
hasanmehedi9034 <mehedi90344@gmail.com>
2022-12-19 22:32:13 +0600

    bfs explore

---
+++ 4_bfs/4_bfs_simulation.cpp
@@ -0,0 +1,13 @@
+/*
+    pseudocode of BFS
+
+    input -> connected graph and a source
+    output -> Traverse all nodes and print the traversal order
+
+    BFS(graph, source)
+
+    - initiate an array called 'Visited' and an empty queue 'q'
+    
+
+
+*/
