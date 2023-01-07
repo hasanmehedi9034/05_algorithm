086fb105f9a1038a5f6dbbe0ff930a4a898c6169
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-07 14:09:44 +0600

    single source and grid traversal

--- 8_bfs_dfs_problem_solving/2_SSSP.cpp
+++ 8_bfs_dfs_problem_solving/2_SSSP.cpp
@@ -1,29 +1,31 @@
-#include <bits/stdc++.h>
+// Problem link - https://cses.fi/problemset/task/1193
+
+#include<bits/stdc++.h>
 using namespace std;
-
-
 const int N = 1e5;
-int visited[N];
-int level[N]; // new line added
 
-vector<int> adj_list[N];
+int visited[N];
+int level[N];
+
+vector<int>adj_list[N];
 
 void bfs(int src) {
     queue<int>q;
 
     visited[src] = 1;
-
+    level[src] = 0; // new line added
     q.push(src);
-    level[src] = 0;
 
     while(!q.empty()) {
         int head = q.front();
         q.pop();
 
-        for (int adj_node: adj_list[head]) {
-            if (visited[adj_node] == 0) {
+        for(int adj_node: adj_list[head]) {
+            if(visited[adj_node] == 0) {
                 visited[adj_node] = 1;
+
                 level[adj_node] = level[head] + 1; // new line added
+
                 q.push(adj_node);
             }
         }
@@ -34,10 +36,9 @@ int main() {
     int nodes, edges;
     cin >> nodes >> edges;
 
-    for (int i = 0; i < edges; i++) {
+    for(int i = 0 ; i < edges ; i++) {
         int u, v;
         cin >> u >> v;
-
         adj_list[u].push_back(v);
         adj_list[v].push_back(u);
     }
@@ -45,18 +46,30 @@ int main() {
     int src = 0;
     bfs(src);
 
-    for (int i = 0; i < nodes; i++) {
-        cout  << "Nodes " << i << "-> level: " << level[i] << endl;
+    for(int i = 0 ; i < nodes ; i++) {
+        cout<<"node "<<i<<" -> level: "<<level[i]<<endl;
     }
+    return 0;
 }
 
 
 
 
 
+/*
+(0)   (1)   (2)
+0 --- 1------2 ---- 3 (3)
+      |      |
+      |      |
+  (2) 5------4 (3)
 
+6 6
 
+0 1
+2 3
+1 5
+1 2
+2 4
+5 4
 
-
-
-
+*/
--- 8_bfs_dfs_problem_solving/4_grid_traversla.cpp
+++ 8_bfs_dfs_problem_solving/4_grid_traversla.cpp
@@ -1,5 +1,5 @@
-/*
 
+/*
 5 8
 ########
 #.A#...#
@@ -7,152 +7,151 @@
 #......#
 ########
 
+Output -> 9
 
-    # -> -1
-    . -> 0
+# -> -1
+. -> 0
+
+(5,6) --> right -> (5,7)
+(5,6) --> left  -> (5, 5)
+(5,6)  -> up ---> (4,6)
+(5,6) ---> down --> (6,6)
+
+(x,y) -> right -> (x, y + 1)
+     -> left   -> (x, y - 1)
+     -> up    ->  (x - 1, y)
+     -> down   -> (x + 1, y)
 
 
+x  y
+-  -
+0  1
+0  -1
+-1  0
+1   0
+
+dx[] = {0, 0, -1, 1}
+dy[] = {1, -1, 0, 0}
+
+for(int i = 0 ; i < 4 ; i++) {
+    int new_x = x + dx[i];
+    int new_y = y + dy[i];
+}
 
 
-    (x, y) -> right (x, y + 1)
-           -> left  (x, y - 1)
-           -> up    (x - 1, y)
-           -> down  (x + 1, y)
+1. is the cell within the maze
+2. is the cell forbidden / wall?
+3. is the cell visited?
 
-    x y
-    0 1
-    0 -1
-    -1 0
-    1 0
 
-    dx[] = {0, 0, -1, 1}
-    dy[] = {1, -1, 0, 0}
-
-    for (int i = 0; i < 4; i++) {
-        int nex_x = x + dx[i];
-        int nex_y = y + dy[i];
-    }
-
-    1. is the cell within the maze
-    2. is the cell forbidden
-    3. is the cell visited
 */
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-
 const int N = 2002;
-int maze[N][N], visited[N][N], level[N][N];
-int n, m;
 
+int maze[N][N], visited[N][N];
+int level[N][N];
+int n, m;
 
 int dx[] = {0, 0, -1, 1};
 int dy[] = {1, -1, 0, 0};
 
-bool is_inside(pair<int, int> co_or) {
-    int x = co_or.first;
-    int y = co_or.second;
 
-    if (x >= 0 && x < n && y >= 0 && y < m) {
+bool is_inside(pair<int, int>coord) {
+    int x = coord.first;
+    int y = coord.second;
+
+    if(x >= 0 && x < n && y >= 0 &&  y < m) {
         return true;
     }
     return false;
 }
 
-bool is_forbidden(pair<int, int> co_or)  {
-    int x = co_or.first;
-    int y = co_or.second;
-
-    if (maze[x][y] = -1) return true;
-    return false;
+bool is_safe(pair<int, int>coord) {
+    int x = coord.first;
+    int y = coord.second;
+    if(maze[x][y] == -1) {
+        return false;
+    }
+    return true;
 }
 
-void bfs(pair<int, int> src) {
-    queue< pair<int, int> > q;
-    
+void bfs(pair<int, int>src) {
+
+    queue< pair<int, int> >q;
     visited[src.first][src.second] = 1;
     level[src.first][src.second] = 0;
 
     q.push(src);
 
     while(!q.empty()) {
-        pair<int, int> head = q.front();
+        pair<int,int>head = q.front();
         q.pop();
-
         int x = head.first;
         int y = head.second;
 
-        for (int i = 0; i < 4; i++) {
-            int nex_x = x + dx[i];
-            int nex_y = y + dy[i];
+        for(int i = 0 ; i < 4 ; i++) {
+            int new_x = x + dx[i];
+            int new_y = y + dy[i];
 
-            pair<int, int> adj_node = {nex_x, nex_y};
+            pair<int,int>adj_node = {new_x, new_y};
 
-            if(is_inside(adj_node) && 
-                !is_forbidden(adj_node) && 
-                visited[nex_x][nex_y] == 0) {
-                    visited[nex_x][nex_y] = 1;
-                    level[nex_x][nex_y] = level[x][y] + 1;
-                    q.push(adj_node);
+            if(is_inside(adj_node) && is_safe(adj_node) && visited[new_x][new_y] == 0) {
+                visited[new_x][new_y] = 1;
+                level[new_x][new_y] = level[x][y] + 1;
+                q.push(adj_node);
             }
         }
     }
 }
 
 int main() {
+
     cin >> n >> m;
 
-    pair<int, int> src, dst;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    pair<int, int>src, dst;
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < n ; j++) {
             level[i][j] = -1;
         }
     }
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0 ; i < n  ; i++) {
         string input;
-        cin >> input;
-        
-        for (int j = 0; j < input.length(); j++) {
-            if (input[j] == '#') {
+        cin>>input;
+        for(int j = 0 ; j < m ; j++) {
+            if(input[j] == '#') {
                 maze[i][j] = -1;
             }
-            else if (input[j] == '.') {
-                maze[i][j] = 0;
-            }
-            else if (input[j] == 'A') {
+            else if(input[j] == 'A') {
                 src = {i, j};
-                maze[i][j] = 2;
             }
-            else if (input[j] == 'B') {
+            else if(input[j] == 'B') {
                 dst = {i, j};
-                maze[i][j] = 3;
             }
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << maze[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    // for(int i = 0 ; i < n ; i++) {
+    //     for(int j = 0; j < m ; j++) {
+    //         cout<<maze[i][j]<<"\t";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<endl;
 
     bfs(src);
 
-    if (level[dst.first][dst.second] == -1) {
-        cout << "No" << endl;
+    if(level[dst.first][dst.second] == -1) {
+        cout<<"NO"<<endl;
     }
     else {
-        cout << "Yes" << endl;
-        cout  << level[dst.first][dst.second] << endl;
+
+        cout<<"YES"<<endl;
+        cout<<level[dst.first][dst.second]<<endl;
     }
 
+
+    return 0;
 }
-
-
-
-
